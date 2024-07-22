de86617739f6e10aaaa616f4889ebc67bc715c81
Emiliano Calderón <78500354+emilioo24@users.noreply.github.com>
2024-07-22 15:41:00 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Servir archivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Ruta principal
+app.get('/', (req, res) => {
+    res.render('views/index');
+});
+
+// Iniciar el servidor
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
